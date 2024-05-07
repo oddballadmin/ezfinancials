f5f73a7a08bc76666fd4b3ef513cdfd3c4fb3a89
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2024-05-06 23:09:42 -0400

    chore: Update .env file with NODE_ENV variable for production deployment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -54,7 +54,7 @@ export const loginUser = async (req, res) => {
             );
 
             res.cookie('token', token, {
-                httpOnly: true,
+                httpsOnly: true,
                 secure: process.env.NODE_ENV === 'production',
 
 
