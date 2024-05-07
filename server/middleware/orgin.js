308800c561ba84243fabedf17e2000e2b0aa84f0
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2024-05-06 23:25:02 -0400

    chore: Update CORS origin options for server and axios requests

--- server/middleware/orgin.js
+++ server/middleware/orgin.js
@@ -1,6 +1,6 @@
 export const orginOptions = [
-    "http://localhost:3000",
-    "https://ezfinancials-s6e4-denr7jmm1-georges-projects-8c72f407.vercel.app",
+    // "http://localhost:3000",
+    // "https://ezfinancials-s6e4-denr7jmm1-georges-projects-8c72f407.vercel.app",
     "https://ezfinancials-s6e4.vercel.app",
-    "http://localhost:8001"
+    // "http://localhost:8001"
 ]
